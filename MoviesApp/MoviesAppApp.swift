099d8608b0f4551435292b1de4aff8a486206487
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-29 17:03:41 +0530

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Sajed Shaikh on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
